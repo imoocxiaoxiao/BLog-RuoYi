8d866ee8e3e54db3068046c9af23467b9cbb31e8
oneonepice <1455941195@qq.com>
2020-06-05 17:28:40 +0800

    ruoyi-admin

---
+++ ruoyi-blog/src/main/java/com/ruoyi/blog/controller/BlogAdminController.java
@@ -0,0 +1,279 @@
+package com.ruoyi.blog.controller;
+
+import com.ruoyi.blog.entity.Blog;
+import com.ruoyi.blog.service.BlogService;
+import com.ruoyi.blog.service.CategoryService;
+import com.ruoyi.blog.service.CommentService;
+import com.ruoyi.blog.service.LinkService;
+import com.ruoyi.blog.service.TagService;
+import com.ruoyi.blog.util.MyBlogUtils;
+import com.ruoyi.blog.util.PageQueryUtil;
+import com.ruoyi.blog.util.Result;
+import com.ruoyi.blog.util.ResultGenerator;
+import com.ruoyi.common.annotation.DataSource;
+import com.ruoyi.common.constant.Constants;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.enums.DataSourceType;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * 博客管理功能
+ *
+ *  @author Lin
+ */
+@Controller
+@RequestMapping("/blog/admin")
+public class BlogAdminController extends BaseController {
+
+
+    private String prefix = "blog";
+
+    @Autowired
+    private BlogService blogService;
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private LinkService linkService;
+    @Autowired
+    private TagService tagService;
+    @Autowired
+    private CommentService commentService;
+
+    // 仪表盘 Dashboard
+    @RequiresPermissions("blog:admin:dash:view")
+    @GetMapping(value = "/dash")
+    public String index(ModelMap mmap) {
+
+        mmap.put("categoryCount", categoryService.getTotalCategories());
+        mmap.put("blogCount", blogService.getTotalBlogs());
+        mmap.put("linkCount", linkService.getTotalLinks());
+        mmap.put("tagCount", tagService.getTotalTags());
+        mmap.put("commentCount", commentService.getTotalComments());
+        return prefix + "/dash";    // 不要轻易使用index
+    }
+
+    // 发布博客
+    @RequiresPermissions("blog:admin:blogs:add")
+    @GetMapping(value = "/edit")
+    public String edit(ModelMap mmap) {
+        mmap.put("categories", categoryService.getAllCategories());
+        return prefix + "/blog_edit";
+    }
+
+    // 博客修改
+    @RequiresPermissions("blog:admin:blogs:edit")
+    @GetMapping(value = "/edit/{blogId}")
+    public String edit(HttpServletRequest request, @PathVariable("blogId") Long blogId) {
+        request.setAttribute("path", "edit");
+        Blog blog = blogService.getBlogById(blogId);
+        if (blog == null) {
+            return "error/404";
+        }
+        request.setAttribute("blog", blogService.getBlogById(blogId));
+        request.setAttribute("categories", categoryService.getAllCategories());
+        return prefix + "/blog_edit";
+    }
+
+    // 博客管理
+    @RequiresPermissions("blog:admin:blogs:view")
+    @GetMapping(value = "/blogs")
+    public String list(HttpServletRequest request) {
+        return prefix + "/blog";
+    }
+
+    // 博客列表 (查)
+    @RequiresPermissions("blog:admin:blogs:list")
+    @GetMapping(value = "/blogs/list")
+    @ResponseBody
+    public Result list(@RequestParam Map<String, Object> params) {
+        if (StringUtils.isEmpty(params.get("page")) || StringUtils.isEmpty(params.get("limit"))) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        PageQueryUtil pageUtil = new PageQueryUtil(params);
+        return ResultGenerator.genSuccessResult(blogService.getBlogsPage(pageUtil));
+    }
+
+    // 保存文章
+    @RequiresPermissions("blog:admin:blogs:save")
+    @PostMapping(value = "/blogs/save")
+    @ResponseBody
+    public Result save(@RequestParam("blogTitle") String blogTitle,
+                       @RequestParam(name = "blogSubUrl", required = false) String blogSubUrl,
+                       @RequestParam("blogCategoryId") Integer blogCategoryId,
+                       @RequestParam("blogTags") String blogTags,
+                       @RequestParam("blogContent") String blogContent,
+                       @RequestParam("blogCoverImage") String blogCoverImage,
+                       @RequestParam("blogStatus") Byte blogStatus,
+                       @RequestParam("enableComment") Byte enableComment) {
+        if (StringUtils.isEmpty(blogTitle)) {
+            return ResultGenerator.genFailResult("请输入文章标题");
+        }
+        if (blogTitle.trim().length() > 150) {
+            return ResultGenerator.genFailResult("标题过长");
+        }
+        if (StringUtils.isEmpty(blogTags)) {
+            return ResultGenerator.genFailResult("请输入文章标签");
+        }
+        if (blogTags.trim().length() > 150) {
+            return ResultGenerator.genFailResult("标签过长");
+        }
+        if (blogSubUrl.trim().length() > 150) {
+            return ResultGenerator.genFailResult("路径过长");
+        }
+        if (StringUtils.isEmpty(blogContent)) {
+            return ResultGenerator.genFailResult("请输入文章内容");
+        }
+        if (blogTags.trim().length() > 100000) {
+            return ResultGenerator.genFailResult("文章内容过长");
+        }
+        if (StringUtils.isEmpty(blogCoverImage)) {
+            return ResultGenerator.genFailResult("封面图不能为空");
+        }
+        Blog blog = new Blog();
+        blog.setBlogTitle(blogTitle);
+        blog.setBlogSubUrl(blogSubUrl);
+        blog.setBlogCategoryId(blogCategoryId);
+        blog.setBlogTags(blogTags);
+        blog.setBlogContent(blogContent);
+        blog.setBlogCoverImage(blogCoverImage);
+        blog.setBlogStatus(blogStatus);
+        blog.setEnableComment(enableComment);
+
+        String saveBlogResult = blogService.saveBlog(blog);
+        if ("success".equals(saveBlogResult)) {
+            return ResultGenerator.genSuccessResult("添加成功");
+        } else {
+            return ResultGenerator.genFailResult(saveBlogResult);
+        }
+    }
+
+    // 更新文章
+    @RequiresPermissions("blog:admin:blogs:edit")
+    @PostMapping("/blogs/update")
+    @ResponseBody
+    public Result update(@RequestParam("blogId") Long blogId,
+                         @RequestParam("blogTitle") String blogTitle,
+                         @RequestParam(name = "blogSubUrl", required = false) String blogSubUrl,
+                         @RequestParam("blogCategoryId") Integer blogCategoryId,
+                         @RequestParam("blogTags") String blogTags,
+                         @RequestParam("blogContent") String blogContent,
+                         @RequestParam("blogCoverImage") String blogCoverImage,
+                         @RequestParam("blogStatus") Byte blogStatus,
+                         @RequestParam("enableComment") Byte enableComment) {
+        if (StringUtils.isEmpty(blogTitle)) {
+            return ResultGenerator.genFailResult("请输入文章标题");
+        }
+        if (blogTitle.trim().length() > 150) {
+            return ResultGenerator.genFailResult("标题过长");
+        }
+        if (StringUtils.isEmpty(blogTags)) {
+            return ResultGenerator.genFailResult("请输入文章标签");
+        }
+        if (blogTags.trim().length() > 150) {
+            return ResultGenerator.genFailResult("标签过长");
+        }
+        if (blogSubUrl.trim().length() > 150) {
+            return ResultGenerator.genFailResult("路径过长");
+        }
+        if (StringUtils.isEmpty(blogContent)) {
+            return ResultGenerator.genFailResult("请输入文章内容");
+        }
+        if (blogTags.trim().length() > 100000) {
+            return ResultGenerator.genFailResult("文章内容过长");
+        }
+        if (StringUtils.isEmpty(blogCoverImage)) {
+            return ResultGenerator.genFailResult("封面图不能为空");
+        }
+        Blog blog = new Blog();
+        blog.setBlogId(blogId);
+        blog.setBlogTitle(blogTitle);
+        blog.setBlogSubUrl(blogSubUrl);
+        blog.setBlogCategoryId(blogCategoryId);
+        blog.setBlogTags(blogTags);
+        blog.setBlogContent(blogContent);
+        blog.setBlogCoverImage(blogCoverImage);
+        blog.setBlogStatus(blogStatus);
+        blog.setEnableComment(enableComment);
+
+        String updateBlogResult = blogService.updateBlog(blog);
+        if ("success".equals(updateBlogResult)) {
+            return ResultGenerator.genSuccessResult("修改成功");
+        } else {
+            return ResultGenerator.genFailResult(updateBlogResult);
+        }
+    }
+
+    // 上传文件
+    @PostMapping("/blogs/md/uploadfile")
+    public void uploadFileByEditormd(HttpServletRequest request,
+                                     HttpServletResponse response,
+                                     @RequestParam(name = "editormd-image-file", required = true)
+                                             MultipartFile file) throws IOException, URISyntaxException {
+        String fileName = file.getOriginalFilename();
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        //生成文件名称通用方法
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        Random r = new Random();
+        StringBuilder tempName = new StringBuilder();
+        tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixName);
+        String newFileName = tempName.toString();
+        //创建文件
+        File destFile = new File(Constants.FILE_UPLOAD_DIC + newFileName);
+        String fileUrl = MyBlogUtils.getHost(new URI(request.getRequestURL() + "")) + "/upload/" + newFileName;
+        File fileDirectory = new File(Constants.FILE_UPLOAD_DIC);
+        try {
+            if (!fileDirectory.exists()) {
+                if (!fileDirectory.mkdir()) {
+                    throw new IOException("文件夹创建失败,路径为：" + fileDirectory);
+                }
+            }
+            file.transferTo(destFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            request.setCharacterEncoding("utf-8");
+            response.setHeader("Content-Type", "text/html");
+            response.getWriter().write("{\"success\": 1, \"message\":\"success\",\"url\":\"" + fileUrl + "\"}");
+        } catch (UnsupportedEncodingException e) {
+            response.getWriter().write("{\"success\":0}");
+        } catch (IOException e) {
+            response.getWriter().write("{\"success\":0}");
+        }
+    }
+
+    // 删除文章
+    @RequiresPermissions("blog:admin:blogs:remove")
+    @PostMapping("/blogs/delete")
+    @ResponseBody
+    public Result delete(@RequestBody Integer[] ids) {
+        if (ids.length < 1) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        if (blogService.deleteBatch(ids)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult("删除失败");
+        }
+    }
+
+
+}
